d9244d77476ef144070fec2b233adb0a89fa4ba3
Ben <br@massa.net>
2023-05-27 20:19:19 +0200

    Initial Commit

---
+++ yt-dl-mac/yt_dl_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  yt_dl_macApp.swift
+//  yt-dl-mac
+//
+//  Created by Ben on 27/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct yt_dl_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ yt-dl-macUITests/yt_dl_macUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  yt_dl_macUITestsLaunchTests.swift
+//  yt-dl-macUITests
+//
+//  Created by Ben on 27/05/2023.
+//
+
+import XCTest
+
+final class yt_dl_macUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
